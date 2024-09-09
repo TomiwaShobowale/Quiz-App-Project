50d86bce5797754a6cb55004673070a29c3ee1ef
TomiwaShobowale <oluwatomiwa.shobowale@gmail.com>
2024-09-09 19:03:10 -0400

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,92 @@
+import 'package:first_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'How is a Cabinet Minister chosen?',
+    [
+      'By the Prime Minister',
+      'By the voters.',
+      'By other Cabinet Ministers.',
+      'By the King.',
+    ],
+  ),
+  QuizQuestion(
+      'What are the territories of Northern Canada and their capital cities?', [
+    'Yukon Territory (Whitehorse), Northwest Territories (Yellowknife), and Nunavut (Iqaluit).',
+    'Northwest Territories (Yellowknife) and Alaska (Juneau).',
+    'Northwest Territories (Yellowknife).',
+    'Alaska (Juneau) and Yukon Territory ( Whitehorse).',
+  ]),
+  QuizQuestion(
+    "What do you mark on a federal election ballot?",
+    [
+      'An "X".',
+      'The number for the candidate.',
+      "The candidate's name.",
+      "The voter's name.",
+    ],
+  ),
+  QuizQuestion(
+    'What happened at the Battle of the Plains of Abraham?',
+    [
+      "The British defeated the French marking the end of France's empire in America",
+      'The Voyagers battled with the British for fur trading rights',
+      'Americans fought the United Empire Loyalists during the American Revolution',
+      'The French defeated the British in a battle for Quebec',
+    ],
+  ),
+  QuizQuestion(
+    'What is a Francophone?',
+    [
+      'A person who speaks French as their first language',
+      'A person who speaks English as their first language',
+      'A smartphone designed by the Canadian company RIM',
+      'The first phone in Canada, invented by Alexander Graham Bell',
+    ],
+  ),
+  QuizQuestion(
+    'What is the capital city of Ontario?',
+    [
+      'Toronto',
+      'Kingston',
+      'Ottawa',
+      'London',
+    ],
+  ),
+  QuizQuestion(
+    'What is the highest military honour a Canadian can receive?',
+    [
+      'Victoria Cross.',
+      'Purple Cross.',
+      'Order of Merit.',
+      'Badge of Courage.',
+    ],
+  ),
+  QuizQuestion(
+    'What is the name of the leader of the Opposition in Ontario?',
+    [
+      'Marit Stiles',
+      'Andrea Horwath',
+      'Peter Tabuns',
+      'Dalton McGuinty',
+    ],
+  ),
+  QuizQuestion(
+    'What is the name of the Lieutenant-Governor of Ontario?',
+    [
+      'Edith Dumont',
+      'James Bartleman',
+      'Elizabeth Dowdeswell',
+      'David C. Onley',
+    ],
+  ),
+  QuizQuestion(
+    'What is the name of the Premier of Ontario?',
+    [
+      'Premier Doug Ford',
+      'Premier Jack Layton',
+      'Premier Kathleen Wynne',
+      'Premier Mike Harris',
+    ],
+  ),
+];
---
+++ lib/make_quiz.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MakeQuiz extends StatelessWidget {
+  const MakeQuiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Column();
+  }
+  }
